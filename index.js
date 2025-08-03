e4199499bc53d30266a0873bb1b84f7fcfb6f673
StrawHatAaron <aaronmiller822028@gmail.com>
2025-08-03 01:05:42 -0700

    its getting late.. trying to make all the svgs show on one page and seperatly with that own dynamic params

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // index.js - Aaron Miller
-
-// Fetch Domestic Auto Production fetchedData from years 1993 - 2025 seasonally adjusted - DAUPSA.csv.
-async function fetchDomesticAutoData() {
-    const data = await d3.csv("DAUPSA.csv").catch(error => {
+//
+// Fetch Consumer Price Indexes for All Urban Consumers - CPIAUCSL.csv.
+async function fetchCPIData() {
+    const data = await d3.csv("CPIAUCSL.csv").catch(error => {
         console.error('CSV load error:', error);
     });
     return data;
 }
+// Fetch Consumer Price Indexes for New Vehicles - CUSR0000SETA01.csv.
+async function fetchCPIDataNewVehicles() {
+    const data = await d3.csv("CUSR0000SETA01.csv").catch(error => {
+        console.error('CSV load error:', error);
+    });
+    return data;
+}
+//
 // Fetch Motor Vehicle Retail Sales: Domestic Autos - DAUTONSA.csv.
 async function fetchDomesticAutoSalesData() {
     const data = await d3.csv("DAUTONSA.csv").catch(error => {
@@ -14,16 +22,23 @@ async function fetchDomesticAutoSalesData() {
     });
     return data;
 }
-
 // Fetch Motor Vehicle Retail Sales: Domestic Light Weight Trucks - DLTRUCKSNSA.csv.
 async function fetchDomesticLightWeightTruckSalesData() {
     const data = await d3.csv("DLTRUCKSNSA.csv").catch(error => {
         console.error('CSV load error:', error);
     });
     return data;
-        }
+}
+//
+// Fetch Domestic Auto Production fetchedData from years 1993 - 2025 seasonally adjusted - DAUPSA.csv.
+async function fetchDomesticAutoData() {
+    const data = await d3.csv("DAUPSA.csv").catch(error => {
+        console.error('CSV load error:', error);
+    });
+    return data;
+}
 
-//MAIN FUNCTION
+//-------------------------------------------------------------MAIN FUNCTION----------------------------------------------------
 async function main() {
     // Declare the chart dimensions and margins.
     const width = 928;
@@ -33,12 +48,22 @@ async function main() {
     const marginBottom = 30;
     const marginLeft = 40;
 
-    // Create the SVG container.
-    const svg = d3.select("svg#chart")
-                    .attr("width", width)
-                    .attr("height", height)
-                    .attr("viewBox", [0, 0, width, height])
-                    .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+    // Create the SVG containers.
+    const svg_cpi = d3.select("svg#graph-cpi")
+                    .attr("width", 0)
+                    .attr("height", 0)
+                    .attr("viewBox", [0, 0, 0, 0])
+                    .attr("style", "max-width:0; height: 0; height: 0;");
+    const svg_sales = d3.select("svg#graph-sales")
+                    .attr("width", 0)
+                    .attr("height", 0)
+                    .attr("viewBox", [0, 0, 0, 0])
+                    .attr("style", "max-width:0; height: 0; height: 0;");
+    const svg_production = d3.select("svg#graph-production")
+                    .attr("width", 0)
+                    .attr("height", 0)
+                    .attr("viewBox", [0, 0, 0, 0])
+                    .attr("style", "max-width: 0; height: 0; height: 0;");
 
     const svg_legend = d3.select("svg#legend")
         .attr("width", 200)
@@ -48,24 +73,35 @@ async function main() {
     const h2 = d3.select("h2")
         .style("text-align", "center");
 
-    // Declare a variable to hold data for Domestic Auto Production (DAUPSA)
-    const domesticAutoData = await fetchDomesticAutoData();
-    console.log("fetchDomesticAutoData returned value", domesticAutoData);
+    // declare variables for Consumer Price Indexes for All Urban Consumers
+    const cpiAllItemsData = await fetchCPIData();
+    // Declare a variable to hold data for Consumer Price Indexes for New Vehicles.
+    const cpiNewVehiclesData = await fetchCPIDataNewVehicles();
 
     // Declare a variable to hold data for Motor Vehicle Retail Sales: Domestic Autos (DAUTONSA).
     const domesticAutoSalesData = await fetchDomesticAutoSalesData();
     // Declare a variable to hold data for Motor Vehicle Retail Sales: Domestic Light Weight Trucks (DLTRUCKSNSA)
     const domesticLightWeightTruckSalesData = await fetchDomesticLightWeightTruckSalesData();
 
+    // Declare a variable to hold data for Domestic Auto Production (DAUPSA)
+    const domesticAutoData = await fetchDomesticAutoData();
+
+
     // Clear the SVG container.
     function clearSVG() {
-        svg.selectAll("*").remove();
+        svg_cpi.selectAll("*").remove();
+        svg_sales.selectAll("*").remove();
+        svg_production.selectAll("*").remove();
         svg_legend.selectAll("*").remove();
         h2.select("*").remove();
-
+        svg_cpi = d3.select("svg#graph-sales");
+        svg_cpi.attr("width", width)
+                .attr("height", height)
+                .attr("viewBox", [0, 0, width, height])
+                .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
     }
 
-    function drawGraphBounds(x, y) {
+    function drawGraphBounds(svg, x, y, label) {
         // Add the X-axis.
         svg.append("g")
             .attr("transform", `translate(0,${height - marginBottom})`)
@@ -83,11 +119,11 @@ async function main() {
                 .attr("y", 10)
                 .attr("fill", "currentColor")
                 .attr("text-anchor", "start")
-                .text("Thousands of Units"));
+                .text(label));
     }
 
     // Function to draw a line and add points with tooltips.
-    function drawLineAddPoints(data, x, y, color, fieldName) {
+    function drawLineAddPoints(svg, data, x, y, color, fieldName, label) {
         // Create a tooltip for displaying data on hover.
         const tooltip = d3.select("body").append("div")
                         .attr("class", "tooltip")
@@ -121,7 +157,7 @@ async function main() {
             .on("mouseover", (event, d) => {
                 tooltip.transition().duration(200).style("opacity", 0.9);
                 tooltip.html(`<strong>Date:</strong> ${d.observation_date}<br>`+
-                            `<strong>Thousands of Units:</strong> ${d[fieldName]}`)
+                            `<strong>${label}:</strong> ${d[fieldName]}`)
                 .style("left", (event.pageX + 10) + "px")
                 .style("top", (event.pageY - 28) + "px");
             })
@@ -130,41 +166,29 @@ async function main() {
             });
     }
 
-    function drawChartDAUPSA() {
-        console.log(domesticAutoData);
-        // Declare the x (horizontal position) scale.
-        const x = d3.scaleUtc(d3.extent(domesticAutoData, d => new Date(d.observation_date)), [marginLeft, width - marginRight]);
-        // Declare the y (vertical position) scale.
-        const y = d3.scaleLinear([0, 600], [height - marginBottom, marginTop]);
-        // Draw the graph bounds.
-        drawGraphBounds(x, y)
-        // Draw the line and points for Domestic Auto Production (DAUPSA).          
-        drawLineAddPoints(domesticAutoData, x, y, "steelblue", "DAUPSA");
-        // Add the title to the chart.
-        h2.text("Domestic Auto Production (DAUPSA) from 1993 to 2025 (Seasonally Adjusted)"); 
-    }
 
-    function drawChartSales() {
-        // Declare the x (horizontal position) scale.
-        const x_auto = d3.scaleUtc(d3.extent(domesticAutoSalesData, d => new Date(d.observation_date)), [marginLeft, width - marginRight]);
-        const x_truck = d3.scaleUtc(d3.extent(domesticLightWeightTruckSalesData, d => new Date(d.observation_date)), [marginLeft, width - marginRight]);
+    // Function to draw the chart for Consumer Price Indexes for All Urban Consumers.
+    function drawChartCPI() {
+        svg_cpi.attr("width", width)
+                .attr("height", height)
+                .attr("viewBox", [0, 0, width, height])
+                .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+        // Declare the x (horizontal position) scale for New Vehicles.
+        const x_auto_CPI = d3.scaleUtc(d3.extent(cpiNewVehiclesData, d => new Date(d.observation_date)), [marginLeft, width - marginRight]);
+        const x_CPI = d3.scaleUtc(d3.extent(cpiAllItemsData, d => new Date(d.observation_date)), [marginLeft, width - marginRight]);
         // Declare the y (vertical position) scale.
-        const y = d3.scaleLinear([50, 1100], [height - marginBottom, marginTop]);
+        const y = d3.scaleLinear([25, 325], [height - marginBottom, marginTop]);
         // Draw the graph bounds.
-        drawGraphBounds(x_auto, y);
+        drawGraphBounds(svg_cpi, x_auto_CPI, y, "CPI Index 1982-1984=100");
         // Draw the line and points for Domestic Auto Sales (DAUTONSA).
-        drawLineAddPoints(domesticAutoSalesData, x_auto, y, "steelblue", "DAUTONSA");
+        drawLineAddPoints(svg_cpi, cpiNewVehiclesData, x_auto_CPI, y, "steelblue", "CUSR0000SETA01", "CPI Index 1982-1984=100");
         // Draw the line and points for Domestic Light Weight Truck Sales (DLTRUCKSNSA).
-        drawLineAddPoints(domesticLightWeightTruckSalesData, x_truck, y, "coral", "DLTRUCKSNSA");
-
-
-
-        // Add a legend to differentiate between the two lines DAUTONSA and DLTRUCKSNSA.
+        drawLineAddPoints(svg_cpi, cpiAllItemsData, x_CPI, y, "coral", "CPIAUCSL", "CPI Index 1982-1984=100");
+        // Add a legend to differentiate between the two lines CUSR0000SETA01 and CPIAUCSL.
         const legend = svg_legend.append("g")
             .attr("transform", "translate(20,20)");
-        const items = [ { color: "steelblue", label: "Domestic Autos (DAUTONSA)" },
-                        { color: "coral", label: "Light Weight Trucks (DLTRUCKSNSA)" }];
-
+        const items = [ { color: "steelblue", label: "New Vehicles in U.S. City Average (CUSR0000SETA01)" },
+                        { color: "coral", label: "All Items in U.S. City Average (CPIAUCSL)" }];
         items.forEach((item, i) => {
             const y = i * 20;
             legend.append("circle")
@@ -179,29 +203,96 @@ async function main() {
                 .style("font-size", "12px")
                 .attr("alignment-baseline", "middle");
         });
+        h2.text("Consumer Price Indexes for All Urban Consumers from 1953 to 2025 (Seasonally Adjusted)");
+    }
 
-        
-
-
+    // Function to draw the chart for Motor Vehicle Retail Sales for Domestic Autos and Domestic Light Weight Trucks.
+    function drawChartSales() {
+            
+        svg_sales.attr("width", width)
+                .attr("height", height)
+                .attr("viewBox", [0, 0, width, height])
+                .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+        // Declare the x (horizontal position) scale.
+        const x_auto = d3.scaleUtc(d3.extent(domesticAutoSalesData, d => new Date(d.observation_date)), [marginLeft, width - marginRight]);
+        const x_truck = d3.scaleUtc(d3.extent(domesticLightWeightTruckSalesData, d => new Date(d.observation_date)), [marginLeft, width - marginRight]);
+        // Declare the y (vertical position) scale.
+        const y = d3.scaleLinear([50, 1100], [height - marginBottom, marginTop]);
+        // Draw the graph bounds.
+        drawGraphBounds(svg_sales, x_auto, y, "Thousands of Units");
+        // Draw the line and points for Domestic Auto Sales (DAUTONSA).
+        drawLineAddPoints(svg_sales, domesticAutoSalesData, x_auto, y, "steelblue", "DAUTONSA", "Thousands of Units");
+        // Draw the line and points for Domestic Light Weight Truck Sales (DLTRUCKSNSA).
+        drawLineAddPoints(svg_sales, domesticLightWeightTruckSalesData, x_truck, y, "coral", "DLTRUCKSNSA", "Thousands of Units");
+        // Add a legend to differentiate between the two lines DAUTONSA and DLTRUCKSNSA.
+        const legend = svg_legend.append("g")
+            .attr("transform", "translate(20,20)");
+        const items = [ { color: "steelblue", label: "Domestic Autos (DAUTONSA)" },
+                        { color: "coral", label: "Light Weight Trucks (DLTRUCKSNSA)" }];
+        items.forEach((item, i) => {
+            const y = i * 20;
+            legend.append("circle")
+                .attr("cx", 0)
+                .attr("cy", y)
+                .attr("r", 6)
+                .style("fill", item.color);
+            legend.append("text")
+                .attr("x", 12)
+                .attr("y", y + 4)
+                .text(item.label)
+                .style("font-size", "12px")
+                .attr("alignment-baseline", "middle");
+        });
         // Add a title to the chart.
         h2.text("Motor Vehicle Retail Sales for Domestic Autos and Domestic Light Weight Trucks from 1967 to 2025 (Not Seasonally Adjusted)"); 
     }
+
+    // Function to draw the chart for Domestic Auto Production (DAUPSA).
+    function drawChartDAUPSA() {
+        svg_production.attr("width", width)
+                        .attr("height", height)
+                        .attr("viewBox", [0, 0, width, height])
+                        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+        // Declare the x (horizontal position) scale.
+        const x = d3.scaleUtc(d3.extent(domesticAutoData, d => new Date(d.observation_date)), [marginLeft, width - marginRight]);
+        // Declare the y (vertical position) scale.
+        const y = d3.scaleLinear([0, 600], [height - marginBottom, marginTop]);
+        // Draw the graph bounds.
+        drawGraphBounds(svg_production, x, y, "Thousands of Units");
+        // Draw the line and points for Domestic Auto Production (DAUPSA).          
+        drawLineAddPoints(svg_production, domesticAutoData, x, y, "steelblue", "DAUPSA", "Thousands of Units");
+        // Add zoom functionality to the chart.
+        // const zoom = d3.zoom()
+        //     .scaleExtent([1, 10])
+        //     .translateExtent([[marginLeft, marginTop], [width - marginRight, height - marginBottom]])
+        //     .on("zoom", (event) => {
+        //         svg.attr("transform", event.transform);
+        //     });
+        // svg.call(zoom);
+        // Add the title to the chart.
+        h2.text("Domestic Auto Production (DAUPSA) from 1993 to 2025 (Seasonally Adjusted)"); 
+    }
+
 
     // Update the SVG based on the selected storyline
     function updateSVG(type) {
         clearSVG();
         switch (type) {
         case "one":
-            drawChartSales();
+            drawChartCPI();
             break;
         case "two":
-            drawChartDAUPSA();
-            break;
-        case "three":
             drawChartSales();
             break;
-        case "four-daupsa":
+        case "three":
             drawChartDAUPSA();
+            break;
+        case "summary":
+            
+            drawChartDAUPSA();
+            drawChartSales();
+            drawChartCPI();
+
             break;
         }
     }
